748dd1c83424a39f5222d9ef726d14732bb4b39a
Gurneet Bhatia <sbgurneet@gmail.com>
2020-06-18 15:58:08 +0400

    added a build

---
+++ build/precache-manifest.73217ce9a251926c5bc941cadb539787.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e40c661ddcf60d1e8efd95836b2c008c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "383581785b849694baed",
+    "url": "/static/css/main.06c0af81.chunk.css"
+  },
+  {
+    "revision": "e8da17e01399d46eafdc",
+    "url": "/static/js/2.287181bd.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.287181bd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "383581785b849694baed",
+    "url": "/static/js/main.7afdb766.chunk.js"
+  },
+  {
+    "revision": "bed7b35f914ee598c417",
+    "url": "/static/js/runtime-main.a947e826.js"
+  },
+  {
+    "revision": "9e4e8857419541b3ad96527f35605080",
+    "url": "/static/media/Diana Dobrescu.9e4e8857.jpg"
+  },
+  {
+    "revision": "465376568d8b91bb7b8ed3e49ad227a6",
+    "url": "/static/media/Gurneet Bhatia.46537656.png"
+  },
+  {
+    "revision": "5a96267e59044f9948c2c0fda9fdb5c6",
+    "url": "/static/media/events_top.5a96267e.jpg"
+  },
+  {
+    "revision": "a7ce31e2dceb76c8a4b1fc82d773e9db",
+    "url": "/static/media/home_top.a7ce31e2.jpeg"
+  }
+]);
